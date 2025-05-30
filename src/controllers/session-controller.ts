dcd415e6de9c5c07070e2d2d26c5734c11111c03
Horquidia <horquidia.a.lima@gmail.com>
2025-05-29 22:16:50 -0300

    Recuperando o Usuário

--- src/controllers/session-controller.ts
+++ src/controllers/session-controller.ts
@@ -1,7 +1,31 @@
 import {Request, Response} from "express";
+import { prisma } from "@/database/prisma";
+import {z} from "zod";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
 
 class SessionsController {
-    create(request: Request, response: Response) {
+    async create(request: Request, response: Response) {
+        const bodySchema = z.object({
+            email: z.string().email(),
+            password: z.string().min(6),
+        })
+
+        const {email, password} = bodySchema.parse(request.body);
+
+        const user = await prisma.user.findFirst({where: {email}});
+
+
+        if(!user) {
+            throw new AppError("Invalid credentials", 401);
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if(!passwordMatch) {
+            throw new AppError("Invalid credentials", 401);
+        }
+        
         return response.json({message: "Session created"}); 
     }
 }
