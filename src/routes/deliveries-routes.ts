0810cbf70bad7984a36163ae366c2cbb36cc4a6c
Horquidia <horquidia.a.lima@gmail.com>
2025-06-01 22:14:43 -0300

    Atualizando Status da Entrega

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { DeliveriesController } from "../controllers/deliveries-controller";
+import { DeliveriesStatusController } from "../controllers/deliveries-status-controller";
 
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
@@ -8,9 +9,11 @@ import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 const deliveriesRoutes = Router();
 
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 deliveriesRoutes.post("/", deliveriesController.create);
 deliveriesRoutes.get("/", deliveriesController.index);
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update);
 
 export { deliveriesRoutes };
