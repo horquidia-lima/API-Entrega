a4795913f5c2328d443884709db9c47296d30192
Horquidia <horquidia.a.lima@gmail.com>
2025-06-03 22:17:29 -0300

    Testando Autenticação

---
+++ src/testes/sessiones-controller.test.ts
@@ -0,0 +1,24 @@
+import resquest from "supertest";
+import { app } from "@/app";
+
+describe("SessionController", () => {
+    let user_id: string;
+
+    it("should authenticate a and get access token", async () => {
+        const userResponse = await resquest(app).post("/users").send({
+            name: "Auth Test User",
+            email: "authtestuser@example.com",
+            password: "password123"
+        })
+
+        user_id = userResponse.body.id
+
+        const sessionsResponse = await resquest(app).post("/sessions").send({
+            email: "authtestuser@example.com",
+            password: "password123",
+        })
+
+        expect(sessionsResponse.status).toBe(200);
+        expect(sessionsResponse.body.token).toEqual(expect.any(String));
+    })
+})
